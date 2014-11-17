9e3fdd3d5d0db800550744bfc00dff6b955062b4
Amanda Murray <greenhatgod@gmail.com>
2014-11-17 13:28:51 -0500

    aaaaaah
    just ignore all the ones that say meta. I did this wrong.

---
+++ Scripts/cameraScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraScript : MonoBehaviour {
+
+	public Transform player;
+	// Update is called once per frame
+	void Update () {
+		transform.position= new Vector3(player.position.x+3,0,-10);
+	}
+}
