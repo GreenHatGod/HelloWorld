a3ba987fb06e13c77edd7d37a4cc7c6c842ab509
Amanda Murray <greenhatgod@gmail.com>
2014-11-17 13:23:13 -0500

    I don't know what I'm doing or what I'm supposed to do. But I made a game for school and I think this is how you post the code...

---
+++ Scripts/gameOver.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameOver : MonoBehaviour {
+
+	void OnGUI (){
+		GUI.Label (new Rect (Screen.width / 2 - 40, 50, 80, 30), "Game Over");
+		GUI.Label (new Rect (Screen.width / 2 - 40, 300,150, 80), "Holy Shovelware, Batman!");
+		if (GUI.Button (new Rect (Screen.width / 2 - 30, 350, 80, 60), "Play again")) {
+			Application.LoadLevel (0);		
+		}
+	}
+}
---
+++ Scripts/move.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+	public bool jump;
+	public int height;
+	//I was trying to do some animations but I couldn't figure out how to make them play, so just ignore these
+
+	void Start () {
+		jump= false;
+		height = 0;
+	}
+	
+
+	void Update () {
+if (Input.GetKey(KeyCode.Space))
+	rigidbody2D.AddForce(new Vector3 (0, 5, 0));
+//this makes you fly, I couldn't get him to just jump
+		jump = true;
+	}
+
+void FixedUpdate(){
+		rigidbody2D.AddForce(new Vector3 (5, 0, 0));
+		//This makes you keep going.
+	}
+
+}
+
